e7b6f364c7665bd18dc881df035158d7d1394976
Nona Rajabi <rajabi.nona@gmail.com>
2022-11-03 14:28:21 +0100

    Intro section completed! Intro section now have all the required images and instructions.

--- gui_base.py
+++ gui_base.py
@@ -37,8 +37,24 @@ class GuiBase:
 
         font_file = [os.path.join(CONFIG.PATHS.STYLES, "OpenSans-Light.ttf")]
         env_img = os.path.join(CONFIG.PATHS.IMGS, "experiment_env.png")
+        env_1 = os.path.join(CONFIG.PATHS.IMGS, "env1.png")
+        env_2 = os.path.join(CONFIG.PATHS.IMGS, "env2.png")
+        env_3 = os.path.join(CONFIG.PATHS.IMGS, "env3.png")
+        env_4 = os.path.join(CONFIG.PATHS.IMGS, "env4.png")
+        hand_1 = os.path.join(CONFIG.PATHS.IMGS, "hand1.png")
+        hand_2 = os.path.join(CONFIG.PATHS.IMGS, "hand2.png")
+        move_1 = os.path.join(CONFIG.PATHS.IMGS, "move1.png")
+        move_2 = os.path.join(CONFIG.PATHS.IMGS, "move2.png")
 
         intro_text = visual.TextStim(
+            font="Open Sans",
+            fontFiles=font_file,
+            alignText="center",
+            win=self.win,
+            units="norm"
+        )
+
+        press_text = visual.TextStim(
             font="Open Sans",
             fontFiles=font_file,
             alignText="center",
@@ -54,29 +70,144 @@ class GuiBase:
             win=self.win
         )
 
+        img1 = visual.ImageStim(
+            units="norm",
+            size=(0.4, 0.7),
+            win=self.win
+        )
+
+        img2 = visual.ImageStim(
+            units="norm",
+            size=(0.4, 0.7),
+            win=self.win
+        )
+
+        img3 = visual.ImageStim(
+            units="norm",
+            size=(0.4, 0.7),
+            win=self.win
+        )
+
+        img4 = visual.ImageStim(
+            units="norm",
+            size=(0.4, 0.7),
+            win=self.win
+        )
+
         intro_text.color = "white"
         intro_text.size = (0.5, 0.15)
         intro_text.wrapWidth = 1.5
+        intro_text.pos = (0, 0.4)
+
+        press_text.text = "(Press Space key to continue)"
+        press_text.color = "white"
+        press_text.size = (0.1, 0.06)
+        press_text.wrapWidth = 1.5
+        press_text.pos = (0, -0.05)
 
         # first page
         intro_text.text = self.intro_text[0]
+
         intro_text.draw()
+        press_text.draw()
         self.win.flip()
-        core.wait(5)
-        event.clearEvents()
+        key = self.kb.waitKeys(keyList=['space'], waitRelease=True)
+        if "space" in key:
+            event.clearEvents()
 
         # second page
         intro_text.text = self.intro_text[1]
+
         intro_text.draw()
+        press_text.draw()
         self.win.flip()
-        core.wait(4)
-        event.clearEvents()
+        key = self.kb.waitKeys(keyList=['space'], waitRelease=True)
+        if "space" in key:
+            event.clearEvents()
 
         # third page
         intro_text.text = self.intro_text[2]
-        intro_text.pos = (0, 0.2)
+
         intro_text.draw()
+        press_text.draw()
         experiment_env.draw()
+        self.win.flip()
+        key = self.kb.waitKeys(keyList=['space'], waitRelease=True)
+        if "space" in key:
+            event.clearEvents()
+
+        # fourth page
+        img1.image = env_1
+        img2.image = env_2
+        img3.image = env_3
+        img4.image = env_4
+
+        img1.pos = (-0.7, -0.5)
+        img2.pos = (-0.25, -0.5)
+        img3.pos = (0.25, -0.5)
+        img4.pos = (0.7, -0.5)
+
+        intro_text.text = self.intro_text[3]
+
+        intro_text.draw()
+        img1.draw()
+        img2.draw()
+        img3.draw()
+        img4.draw()
+        press_text.draw()
+        self.win.flip()
+        key = self.kb.waitKeys(keyList=['space'], waitRelease=True)
+        if "space" in key:
+            event.clearEvents()
+
+        # fifth page
+        img2.image = move_1
+        img3.image = move_2
+
+        intro_text.text = self.intro_text[4]
+        intro_text.size = (0.2, 0.1)
+
+        intro_text.draw()
+        press_text.draw()
+        img2.draw()
+        img3.draw()
+        self.win.flip()
+        key = self.kb.waitKeys(keyList=['space'], waitRelease=True)
+        if "space" in key:
+            event.clearEvents()
+
+        # sixth page
+        img2.image = hand_1
+        img3.image = hand_2
+
+        intro_text.text = self.intro_text[5]
+        intro_text.size = (0.2, 0.1)
+
+        intro_text.draw()
+        press_text.draw()
+        img2.draw()
+        img3.draw()
+        self.win.flip()
+        key = self.kb.waitKeys(keyList=['space'], waitRelease=True)
+        if "space" in key:
+            event.clearEvents()
+
+        # seventh page
+        intro_text.text = self.intro_text[6]
+        intro_text.size = (0.5, 0.15)
+
+        intro_text.draw()
+        press_text.draw()
+        self.win.flip()
+        key = self.kb.waitKeys(keyList=['space'], waitRelease=True)
+        if "space" in key:
+            event.clearEvents()
+
+        # eighth page
+        intro_text.text = self.intro_text[7]
+
+        intro_text.draw()
+        press_text.draw()
         self.win.flip()
         key = self.kb.waitKeys(keyList=['space'], waitRelease=True)
         if "space" in key:
--- instructions_gui.py
+++ instructions_gui.py
@@ -1,8 +1,17 @@
 from gui_base import GuiBase
 
-intro_text = ["In this experiment, you will either hand objects to the robot or move them to another location.",
-              "The instructions for each trial comes visually with that trial.",
-              "Now let\'s start!\n Press Spece Key When You Are Ready"]
+intro_text = ["Hello dear participant! Welcome to our experiment!",
+              "In this experiment, you will either hand objects to the robot or move them to another location.",
+              "There are four available object places in front of the robot.",
+              "Two of these object places (one in the left and one in the right) is occupied by objects.",
+              "If the instruction is to move one of the objects, it will look like one of the images below.\n"
+              "For example, if the arrow is on the left side, based on the object\'s current location, i.e., A or C, "
+              "you should move it to the other location, i.e., C or A.",
+              "If the instruction is to hand the object to the robot, it will look like one of the images below.\n"
+              "For example, if the arrow is in the left, you pick the object from its current location (A or C), and "
+              "hand it to the robot.",
+              "The instructions for each trial will be visually provided to you at the beginning that trial.",
+              "Now let\'s start!\n Press Space Key When You Are Ready"]
 
 if __name__ == "__main__":
     gui = GuiBase(intro_text)
